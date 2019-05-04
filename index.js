0fbc551efd6c7ec72234d3f93f4632d6dae7146a
Jakkzero <50273567+Jakkzero@users.noreply.github.com>
2019-05-04 19:30:39 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,27 @@
 const Discord = require('discord.js');
 const bot = new Discord.Client();
-
-const token = 'NTc0MjU2MDczOTgxMjk2NjYw.XM23GQ.ppHYOotcFx4ZrkRds3ULm7bsOac';
-
+ 
+const token = '';
+ 
 bot.on('ready', () =>{
     console.log('This bot is online!');
-    bot.user.setActivity('Subscribe to my channel! youtube.com/mrzlionz :) | coming soon | Try l!help for some help uwu |')
-})
-
-bot.on('message', msg=>{
-    if(msg.content ===  "HELLO"){
+    bot.user.setActivity('Subscribe to my channel! youtube.com/mrzlionz :) | Try l!help for some help uwu')
+ 
+    if(msg.content ===  "l!hello"){
         msg.reply('Whats good nibba');
     }
-})
 
+    if(msg.content ===  "l!help"){
+        msg.reply("The Lionz-Bot github page and its commands can be found at: <https://github.com/Jakkzero/LionzBot>");
+    }
+ 
+    if(msg.content ===  "l!test"){
+        msg.reply("This bot is working as it's supposed to! (probably)");
+    }
+ 
+    if(msg.content ===  "l!balanced"){
+        msg.reply('Perfectly balanced, like all things should be. :thanosxd:')
+    }
+})
+ 
 bot.login(token);
